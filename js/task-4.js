c38fe2e9a3d692556542153853aaef84a0e27e77
Anna Sokolova <sokolovaskorik@gmail.com>
2020-11-16 15:00:17 +0200

    Add solution task 4

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const decrBtnRef = document.querySelector('button[data-action="decrement"]');
+// console.log(decrBtnRef);
+const incrBtnRef = document.querySelector('button[data-action="increment"]');
+// console.log(incrBtnRef);
+const valueRef = document.querySelector('#value');
+// console.log(valueRef);
+const counter = document.querySelector('#counter');
+let counterValue = 0;
+
+const incrementFn = () => {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+};
+
+const decrementFn = () => {
+  counterValue > 0 ? (counterValue -= 1) : (counterValue = 0);
+  valueRef.textContent = counterValue;
+};
+
+decrBtnRef.addEventListener('click', decrementFn);
+incrBtnRef.addEventListener('click', incrementFn);
